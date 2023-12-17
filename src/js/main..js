a2ecd34a495bd2dc14f3945530465df8a6c36ccb
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-12-17 17:13:44 +0530

    feat: add ui

--- src/js/main..js
+++ src/js/main..js
@@ -1,1 +1,31 @@
-console.log("Hi");
+// const formEl = document.forms.inputNames;
+// console.log(formEl);
+// const contactDetails = [];
+// formEl.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const fullName = formEl.fullname.value;
+//   const email = formEl.email.value;
+//   const phone = formEl.phone.value;
+//   const streetAdr = formEl.streetAddress.value;
+//   const city = formEl.city.value;
+//   addContactDetails(fullName, email, phone, streetAdr, city);
+//   console.log(contactDetails);
+//   formEl.fullname.value = "";
+//   formEl.email.value = "";
+//   formEl.phone.value = "";
+//   formEl.streetAddress.value = "";
+//   formEl.city.value = "";
+// });
+
+// function addContactDetails(fullName, email, phone, streetAdr, city) {
+//   contactDetails.push({
+//     id: contactDetails.length,
+//     fullName: fullName,
+//     email: email,
+//     phone: phone,
+//     streetAdr: streetAdr,
+//     city: city,
+//   });
+// }
+
+// function showDetails() {}
