1a3f36c5a63256e43b3d177fe7c5870a4594f41b
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-12-18 20:34:58 +0530

    feat: adding functionalities

--- src/js/main..js
+++ src/js/main..js
@@ -1,31 +1,73 @@
-// const formEl = document.forms.inputNames;
-// console.log(formEl);
-// const contactDetails = [];
-// formEl.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const fullName = formEl.fullname.value;
-//   const email = formEl.email.value;
-//   const phone = formEl.phone.value;
-//   const streetAdr = formEl.streetAddress.value;
-//   const city = formEl.city.value;
-//   addContactDetails(fullName, email, phone, streetAdr, city);
-//   console.log(contactDetails);
-//   formEl.fullname.value = "";
-//   formEl.email.value = "";
-//   formEl.phone.value = "";
-//   formEl.streetAddress.value = "";
-//   formEl.city.value = "";
-// });
+const addContactBtnEl = document.querySelector("#add-contact");
+const modalEl = document.querySelector("#modal");
+const modalClosebtn = document.querySelector("#modal-close-btn");
+const formEl = document.forms.inputForm;
+const contactList = document.querySelector("#contact-list");
+console.log(formEl);
+console.log(modalClosebtn);
 
-// function addContactDetails(fullName, email, phone, streetAdr, city) {
-//   contactDetails.push({
-//     id: contactDetails.length,
-//     fullName: fullName,
-//     email: email,
-//     phone: phone,
-//     streetAdr: streetAdr,
-//     city: city,
-//   });
-// }
+// console.log(addContactBtnEl, modalEl);
 
-// function showDetails() {}
+let datas = [];
+console.log(datas.length);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(formEl);
+  console.log(...formData);
+  const getFormData = Object.fromEntries(formData);
+  console.log(getFormData);
+  transferToDatabase(getFormData);
+  formEl.reset();
+  closeModal();
+};
+
+const handleFormdata = (e) => {
+  console.log("formdata fired");
+  console.log(e);
+  const formData = e.formData;
+
+  formData.append("id", datas.length + 1);
+};
+
+function transferToDatabase(data) {
+  datas.push(data);
+  contactList.innerHTML = "";
+  datas.forEach((dataobj) => showData(dataobj));
+  console.log(datas);
+}
+
+function showData(data) {
+  const tableRow = document.createElement("tr");
+  tableRow.classList.add("hover:bg-orange-400", "cursor-pointer");
+  tableRow.setAttribute("data-id", data.id);
+  tableRow.innerHTML = `
+    
+    <td class="py-4">${data.id}</td>
+    <td class="font-semibold text-gray-700">${data.firstname}</td>
+    <td class="text-center">${data.streetAdr}</td>
+    <td>
+      <span class="bg-slate-200 px-2 py-1 rounded text-gray-700"
+        >${data.label}</span
+      >
+    </td>
+    <td>${data.email}
+    </td>
+    <td>${data.phone}</td>
+  `;
+  contactList.appendChild(tableRow);
+}
+
+formEl.addEventListener("submit", handleSubmit);
+formEl.addEventListener("formdata", handleFormdata);
+
+function openModal() {
+  modalEl.classList.remove("hidden");
+}
+function closeModal() {
+  modalEl.classList.add("hidden");
+}
+
+addContactBtnEl.addEventListener("click", openModal);
+modalClosebtn.addEventListener("click", closeModal);
